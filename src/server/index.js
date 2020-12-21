4892da1b3ce692cfd253c905c2864ac398741ff7
Timothy LeBon <timothy.lebon@diconium.com>
2020-12-21 23:30:59 +0100

    feat: Included rover get calls
    Still sort of in conceptualization mode, I created the get route for the rover on the BE, need to think about how the state will work and how I want to store the data for the calls. I had planned to just do an initial load but im not sure if thats the solution i want now.ah.. maybe promise.all

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,47 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
 
 // example API call
 app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
+	try {
+		let image = await fetch(
+			`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
+		).then((res) => res.json());
+		res.send({ image });
+	} catch (err) {
+		console.log('error:', err);
+	}
+});
+// API calls for rover manifest and photos
+// grabs the most recent date from manifest and then pull that date from photos.
+app.get('/rover_data/:rover', async (req, res) => {
+	const rover = req.params.rover;
+	try {
+		let roverData = await fetch(
+			`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.API_KEY}`
+		).then((res) => res.json());
+		const { max_sol } = roverData;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+		let roverPhotos = await fetch(
+			`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${max_sol}&api_key=${process.env.API_KEY}`
+		).then((res) => res.json());
+		res.send({ roverData, roverPhotos });
+	} catch (err) {
+		console.log('error:', err);
+	}
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
